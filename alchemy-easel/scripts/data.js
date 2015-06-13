c4c0e4a744871814a41471e89d9acd8a0616da68
Peter Gagliardi <ptrgags@live.com>
2015-06-13 20:13:37 +0000

    Added Data file
    Redesigned data in a a new, simpler format. This may change, but
    it is more compact than the last version

---
+++ alchemy-easel/scripts/data.js
@@ -0,0 +1,277 @@
+﻿//Generate an array of angle coordinates
+//for a shape of any radius. units are in turns
+//Returns an array of length `sides` with
+//the coordinates
+var regularShape = function(sides) {
+	var shape = [];
+	for (var i = 0; i < sides; i++)
+		shape.push(i / sides);
+	return shape;
+}
+
+//Create a point with duplicate vertices
+//Angle must be in turns
+var nullShape = function(vertices, angle) {
+	var shape = [];
+	for (var i = 0; i < vertices; i++)
+		shape.push(angle);
+	return shape;
+}
+
+//Return a shape-like arc
+var arcShape = function(startAngle, endAngle) {
+	return [startAngle, endAngle];
+}
+
+//return a circle shape
+var circleShape = function(startAngle) {
+	return arcShape(startAngle, startAngle + 1);
+}
+
+//Rotate a shape's coordinates.
+//angle must be positive.
+var rotateShape = function(shape, angle) {
+	var outShape = [];
+	for (var i in shape) {
+		var point = (shape[i] + angle) % shape.length;
+		outShape.push(point);
+	}
+	return outShape;
+}
+
+//Cycle a shape's coordinates a number
+//of places. places must be positive
+var cycleVertices = function(shape, places) {
+	var outShape = [];
+	for (var i in shape) {
+		var point = shape[(i + places) % shape.length];
+		outShape.push(point);
+	}
+	return outShape;
+}
+
+//Duplicate points in a shape a number
+//of times. ex. [1, 2, 3], 2 -> [1, 1, 2, 2, 3, 3]
+var duplicatePoints = function(shape, times) {
+	var outShape = [];
+	for (var i in shape) {
+		for (var j = 0; j < times; j++)
+			outShape.push(shape[i]);
+	}
+	return outShape;
+}
+
+//repeat the points in a shape a number of 
+//times. ex. [1, 2, 3], 2 -> [1, 2, 3, 1, 2, 3]
+var repeatePoints = function(shape, times)
+{
+	var outShape = [];
+	for (var i = 0; i < times; i++) {
+		for (var j in shape)
+			outShape.push(shape[j]);
+	}
+	var outShape;
+}
+
+//basic shapes. 
+//Units are in turns
+var HEXAGON = regularShape(6);
+var TRIANGLE = regularShape(4);
+var SQUARE = regularShape(4);
+var CIRCLE = circleShape(0);
+
+//Special shapes for this animation
+var CIRCLE2 = circleShape(1/2);
+var TRIANGLE_4 = [1/12, 5/12, 5/12, 9/12]; //Duplicate second point
+var TRIANGLE_DOWN = rotateShape(TRIANGLE, 1/12);
+var TRIANGLE_UP = rotateShape(TRIANGLE, 1/4);
+var TRIANGLE_6 = duplicatePoints(TRIANGLE_UP, 2); //duplicate all points
+
+
+var PHASES = [
+	//Trace out two concentric circles
+	[
+		['anim', 'arc', 'outer', CIRCLE],
+		['anim', 'arc', 'inner', CIRCLE2]
+	],
+	//Create a diamond at the four cardinal directions
+	[
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape', 'outer', nullShape(4, 0), SQUARE]
+	],
+	//Turn diamond into an equliateral triangle pointing downwards
+	[
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape', 'outer', SQUARE, TRIANGLE_4]
+	],
+	//Add a second triangle
+	[	
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['static', 'shape', 'outer', TRIANGLE_DOWN],
+		['anim', 'shape', 'inner', nullShape(3, 0), TRIANGLE_UP]
+	],
+	//Rotate outer triangle, inner triangle
+	//morphs into a hexagon
+	[	
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape', 'outer', TRIANGLE_DOWN, TRIANGLE_UP],
+		['anim', 'shape', 'inner', TRIANGLE_6, HEXAGON]
+	],
+	//Both exit to the top
+	[	
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape', 'outer', TRIANGLE_UP, nullShape(3, 1/4)],
+		['anim', 'shape', 'inner', HEXAGON, nullShape(6, 1/4)]
+	],
+	//Enter a hexagon with diagonals from the top
+	[	
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape-diag', 'inner', nullShape(6, 1/4), HEXAGON],
+	],
+	//Hexagon exits at the top, triangle with radials enters from right
+	[	
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape-diag', 'inner', nullShape(6, 1/4), HEXAGON],
+		['anim', 'shape-rad', 'outer', nullShape(3, 0), TRIANGLE_DOWN],
+	],
+	//Triangle exits to the right
+	[	
+		['static', 'arc', 'outer', CIRCLE],
+		['static', 'arc', 'inner', CIRCLE2],
+		['anim', 'shape-rad', 'outer', TRIANGLE_DOWN, nullShape(3, 1/2)]
+	],
+	//Un-trace the circles
+	[	
+		['anim-rev', 'arc', 'outer', CIRCLE],
+		['anim-rev', 'arc', 'inner', CIRCLE2]
+	]
+];
+
+/*
+var PHASES = [
+	//Trace out two concentric circles
+	[
+		['circle', 'outer', [0, 1, 'ccw']],
+		['circle', 'inner', [1/2, 3/2, 'ccw']]
+	],
+	//Create a diamond at the four cardinal directions
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape', 'outer', [
+			[0, 0, 'ccw'],
+			[0, 1/4, 'ccw'],
+			[0, 1/2, 'ccw'],
+			[0, 3/4, 'ccw']
+		]]
+	],
+	//Turn diamond into an equilateral triangle pointing downwards
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape', 'outer', [
+			[0, 1/12, 'ccw'],
+			[1/4, 5/12, 'ccw'],
+			[1/2, 5/12, 'cw'],
+			[3/4, 3/4, 'ccw']
+		]]
+	],
+	//Add a second triangle the opposite way
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape-static', 'outer', [1/12, 5/12, 3/4]],
+		['shape', 'inner', [
+			[0, 1/4, 'ccw'], 
+			[0, 7/12, 'ccw'],
+			[0, 11/12, 'ccw']
+		]]
+	],
+	//Rotate the outer triangle so it points upward.
+	//make the inner triangle into a regular hexagon
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape', 'outer', [
+			[1/12, 1/4, 'ccw'],
+			[5/12, 7/12, 'ccw'],
+			[3/4, 11/12, 'ccw']
+		]],
+		['shape', 'inner', [
+			[1/4, 2/6, 'ccw'],
+			[1/4, 1/2, 'ccw'],
+			[7/12, 2/3, 'ccw'],
+			[7/12, 5/6, 'ccw'],
+			[11/12, 1, 'ccw'],
+			[11/12, 1/6, 'ccw']
+		]]
+	],
+	//All points exit at the top of the circle
+    [
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape', 'outer', [
+			[1/4, 1/4, 'ccw'],
+			[7/12, 1/4, 'cw'],
+			[11/12, 1/4, 'ccw']
+		]],
+		['shape', 'inner', [
+			[2/6, 1/4, 'cw'],
+			[1/2, 1/4, 'cw'],
+			[2/3, 1/4, 'cw'],
+			[5/6, 1/4, 'ccw'],
+			[1, 1/4, 'ccw'],
+			[1/6, 1/4, 'ccw']
+		]]
+	],
+	//Hexagon with diagonals enters from top
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape-diagonals', 'inner', [
+			[1/4, 1/3, 'ccw'],
+			[1/4, 1/2, 'ccw'],
+			[1/4, 2/3, 'ccw'],
+			[1/4, 5/6, 'cw'],
+			[1/4, 0, 'cw'],
+			[1/4, 1/6, 'cw']
+		]]
+	],
+	//Hexagon exits at top
+	//Enter triangle from right with radials
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape-diagonals', 'inner', [
+			[1/3, 1/4, 'cw'],
+			[1/2, 1/4, 'cw'],
+			[2/3, 1/4, 'cw'],
+			[5/6, 1/4, 'ccw'],
+			[0, 1/4, 'ccw'],
+			[1/6, 1/4, 'ccw']
+		]],
+		['shape-radials', 'outer', [
+			[0, 1/12, 'ccw'],
+			[0, 5/12, 'ccw'],
+			[0, 3/4, 'cw']
+		]]
+	],
+	//Triangle exits to the left
+	[
+		['circle-static', 'outer'],
+		['circle-static', 'inner'],
+		['shape-radials', 'outer']
+	],
+	//Un-trace the circles
+	[
+		['circle', 'outer', [0, 1, 'ccw-inv']],
+		['circle', 'outer', [1/2, 3/2, 'ccw-inv']]
+	]
+];*/
